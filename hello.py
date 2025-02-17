f093d02b1a4068efd9dfb8b180fb930a04fe3352
MArcdionisio <marcanthonydionisio18@gmail.com>
2025-02-17 18:48:25 +0800

    My first Python program

---
+++ hello.py
@@ -0,0 +1,9 @@
+Name = "Marc Dionisio"
+Course = "BSIT"
+Interests_in_programming = "Coding"
+
+print("Hello world!")
+
+print("Name: " + Name)
+print("Course: " + Course)
+print("Interests in programming: " + Interests_in_programming) 
